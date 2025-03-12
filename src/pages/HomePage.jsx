ce9160e0b1b1aa20df22d16f4ebc87eb97ac3a60
Fede <fede.atzeni372@gmail.com>
2025-03-12 15:06:09 +0100

    feat axios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,23 @@
+import axios from "axios"
+
+import { useState, useEffect } from "react"
+
 import MovieCard from "../components/MovieCard"
 
 export default function HomePage() {
+
+    const [movies, setMovies] = useState([])
+
+    function fetchMovies() {
+        axios.get("http://localhost:3000/api/movies/")
+            .then(
+                res => console.log(res.data)
+            )
+            .catch(err => console.log(err))
+    }
+
+    fetchMovies()
+
     return (
         <>
             <h1>Movies</h1>
